c2369fb39e5f9237bd0c659e6e709693e7876e16
seiyria <kyle@seiyria.com>
2015-05-05 21:42:36 -0500

    timeout before trying to load gallerygrid

--- dist/js/angular-gallerygrid.js
+++ dist/js/angular-gallerygrid.js
@@ -1,11 +1,15 @@
 (function() {
-  angular.module('hs.gallerygrid', []).directive('gallerygrid', function() {
-    return {
-      restrict: 'A',
-      link: function(scope, element, attrs) {
-        return scope.gallerygrid = new GalleryGrid(element[0]);
-      }
-    };
-  });
+  angular.module('hs.gallerygrid', []).directive('gallerygrid', [
+    '$timeout', function($timeout) {
+      return {
+        restrict: 'A',
+        link: function(scope, element, attrs) {
+          return $timeout(function() {
+            return scope.gallerygrid = new GalleryGrid(element[0]);
+          }, 1000);
+        }
+      };
+    }
+  ]);
 
 }).call(this);
